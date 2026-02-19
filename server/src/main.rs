3e7288ea48c8cca0e0ec7bd18a6a2b07b3caf3a5
jantibur <aljchrtib@protonmail.com>
2026-02-19 11:03:29 +0800

    Push progress

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,43 +1,79 @@
-use axum::{Json, Router, routing::get};
-use serde_json::{json, Value};
+mod card;
+mod balance;
+mod product;
+
+
+use axum::{serve, Router, routing::post, routing::get};
 use std::net::SocketAddr;
 use tower_http::cors::{CorsLayer, Any};
+use sqlx::sqlite::{SqlitePool, SqliteConnectOptions, SqliteJournalMode};
+use std::str::FromStr;
+use tokio::net::TcpListener;
+use card::{handle_make_card_lost, handle_generate_card};
+use balance::{handle_add_balance, handle_total_balance};
+use product::{handle_add_product, handle_add_product_inventory, handle_get_products};
 
-async fn root_handler() -> Json<Value>
-{
-    Json(json!({"message": "Welcome to the root of PUSIT server"}))
+
+#[derive(Clone)]
+struct AppState {
+    database: SqlitePool,
 }
 
 #[tokio::main]
-async fn main() 
+async fn
+main() -> Result<(), Box<dyn std::error::Error>>
 {
-    // Modify this line in production
-    // So that the only thing that can communicate with this server are:
-    // Controller, and Terminals!
-
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods(Any);
 
-
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-
-    println!("[PUSIT-SERVER] Hosting Server On: {}", addr);
+    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
    
+    let options = SqliteConnectOptions::from_str("sqlite:server.db")?
+        .journal_mode(SqliteJournalMode::Wal)
+        .create_if_missing(true);
+
+    let pool = SqlitePool::connect_with(options).await?;
+   
+    let state = AppState { database: pool };
+
+    let query = "
+        CREATE TABLE IF NOT EXISTS card (
+            is_lost INTEGER,
+            uuid TEXT PRIMARY KEY,
+            uid BLOB UNIQUE,
+            server_uid BLOB,
+            balance BOOLEAN 
+        );
+        CREATE TABLE IF NOT EXISTS product (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            price INTEGER,
+            inventory INTEGER
+        );
+        CREATE TABLE IF NOT EXISTS transact (
+            uuid TEXT PRIMARY KEY,
+            product TEXT,
+            price INTEGER
+        );
+    ";
+
+    sqlx::query(query).execute(&state.database).await?;
+
     let app = Router::new()
-        .route("/", get(root_handler))
-        .layer(cors); 
+        .route("/make-card-lost", post(handle_make_card_lost))
+        .route("/generate-card", post(handle_generate_card))
+        .route("/add_balance", post(handle_add_balance))
+        .route("/total_balance", get(handle_total_balance))
+        .route("/add_product", post(handle_add_product))
+        .route("/add_product_inventory", post(handle_add_product_inventory))
+        .route("/get_products", get(handle_get_products))
+        .with_state(state)
+        .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("[SERVER] Hosting functions at: {}", addr);
 
-    axum::serve(listener, app).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
+    serve(listener, app).await?;
+    Ok(())
 }
-
-
-
-
-
-
-
-
-
